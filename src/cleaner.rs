238bd6a7f9a99943bb4fabdb4ec9aeacc6504914
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-06-08 18:31:08 +0200

    working cleaning tags

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -1,5 +1,9 @@
 use crate::image_manager::Image;
 
-pub fn clean_exif(buffer: &mut [u8], image_data: &mut Image) {
-
+pub fn clean_exif(buffer: &mut Vec<u8>, image_data: &mut Image) {
+    let start = image_data.ifd0_segment_start;
+    let end =  image_data.exif_segment_size as usize;
+    for i in start..end {
+        buffer[i] = 0;
+    }
 }
