519b678dca55aa4d7fdb7c54b2e91587eb5e1feb
Radosław Rolka <radoslaw.rolka@gmail.com>
2024-06-18 20:55:31 +0200

    added file outputs examples

--- run_test_files.py
+++ run_test_files.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == "__main__":
     for file in os.listdir("./resources/images/"):
-        os.system(f"cargo run ./resources/images/\"{file}\" -c -n ./resources/cleaned/\"{file}\"")
+        os.system(f"cargo run ./resources/images/\"{file}\" -c -n ./resources/cleaned/\"{file}\" -o ./resources/outputs/\"{file[:-4]}\".txt")
         os.system(f"cargo run ./resources/cleaned/\"{file}\" -p")
